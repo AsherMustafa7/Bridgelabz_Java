bfa16dfb475549ecbe1c04abacacc5f1fc08678e
Asher Mustafa <ashermustafa7@gmail.com>
2026-09-25 00:45:26 +0530

    [Asher Mustafa]: Putting some changes for the Factorsofnumber.java file in level 1 of javaArray

--- src/main/java/javaArray/Level1/FactorsOfNumber.java
+++ src/main/java/javaArray/Level1/FactorsOfNumber.java
@@ -25,7 +25,7 @@ public class FactorsOfNumber {
 
         int maxFactor = 10;
         int[] factors = new int[maxFactor];
-        int index = 0;
+        int index = 0;  
 
         for (int i = 1; i <= number; i++) {
             if (number % i == 0) {
